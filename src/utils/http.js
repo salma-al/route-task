f84329b8ab4f1dc2901a14b81f0419bc5a78f7ad
Salma Ali <salma_fouad@outlook.com>
2024-07-14 20:39:33 +0300

    url

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,13 +6,13 @@ const url = 'https://route-json-server-salma-als-projects.vercel.app';
 export const queryClient = new QueryClient();
 
 export async function getCustomers() {
-  const res = await axios.get(`${url}/customers`);
+  const res = await axios.get(`https://route-json-server-salma-als-projects.vercel.app/customers`);
 
   return res.data;
 }
 
 export async function getTransactions() {
-  const res = await axios.get(`${url}/transactions`);
+  const res = await axios.get(`https://route-json-server-salma-als-projects.vercel.app/transactions`);
 
   return res.data;
 }
